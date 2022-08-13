48a568f955415957088d50695e75dd7de7807e5c
James Oulman <joulman@gmail.com>
2022-08-13 14:41:19 -0400

    fix output in consul

--- consul/consul.tf
+++ consul/consul.tf
@@ -1,5 +1,5 @@
 resource "hcp_consul_cluster" "consul" {
-  hvn_id          = data.terraform_remote_state.global.outputs.hcp_hvn_aws_use1_id
+  hvn_id          = data.terraform_remote_state.global.outputs.hcp_hvn_aws_use1_hvn_id
   cluster_id      = var.cluster_id
   tier            = var.tier
   public_endpoint = var.public_endpoint
