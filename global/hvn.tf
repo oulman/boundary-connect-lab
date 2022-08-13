a12f52e0305ab8849c285daa59aadf25df7b22e4
James Oulman <oulman@users.noreply.github.com>
2022-08-13 13:50:41 -0400

    Add hcp vpc peering (#4)
    * Add HCP provider and fix formatting
    
    * fix hcp_cloud_provider var name
    
    * fix var names
    
    * fix hcp provider names
    
    * Add HVN to VPC peering
    
    * add routes to all tables

--- global/hvn.tf
+++ global/hvn.tf
@@ -16,3 +16,20 @@ resource "hcp_aws_network_peering" "hcp_use1_vpc" {
   peer_account_id = module.vpc.vpc_owner_id
   peer_vpc_region = data.aws_arn.vpc.region
 }
+
+resource "aws_vpc_peering_connection_accepter" "hcp_use1_vpc" {
+  vpc_peering_connection_id = hcp_aws_network_peering.hcp_use1_vpc.provider_peering_id
+  auto_accept               = true
+}
+
+data "aws_route_tables" "vpc" {
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_route" "hvn_peer_route" {
+  count                     = length(data.aws_route_tables.vpc.ids)
+  route_table_id            = tolist(data.aws_route_tables.vpc.ids)[count.index]
+  destination_cidr_block    = hcp_hvn.aws_use1_hvn.cidr_block
+  vpc_peering_connection_id = hcp_aws_network_peering.hcp_use1_vpc.provider_peering_id
+}
+
