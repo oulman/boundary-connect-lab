60dd94e7fb8c71917e478959402e61a870667a4b
James Oulman <joulman@gmail.com>
2022-08-13 14:25:36 -0400

    Create HCP consul cluster

---
+++ consul/_config.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "remote" {
+    organization = "jamesoulman"
+
+    workspaces {
+      name = "boundary-connect-lab-consul"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "0.40.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+provider "hcp" {
+  # Configuration options
+}
+
+data "terraform_remote_state" "global" {
+  backend = "remote"
+
+  config = {
+    organization = "jamesoulman"
+    workspaces = {
+      name = "global"
+    }
+  }
+}
