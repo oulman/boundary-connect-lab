ec73ac1748654a9333c6c584a4b9bfdd0dd2ec92
James Oulman <joulman@gmail.com>
2022-08-13 12:05:02 -0400

    fix var names

--- global/hvn.tf
+++ global/hvn.tf
@@ -11,7 +11,7 @@ resource "hcp_hvn" "aws_use1_hvn" {
 
 resource "hcp_aws_network_peering" "hcp_use1_vpc" {
   hvn_id          = hcp_hvn.aws_use1_hvn.hvn_id
-  peering_id      = var.hvn_peering_id
+  peering_id      = var.hcp_hvn_peering_id
   peer_vpc_id     = module.vpc.vpc_id
   peer_account_id = module.vpc.vpc_owner_id
   peer_vpc_region = data.aws_arn.vpc.region
