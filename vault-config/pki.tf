c3578774ca3958978fc3561a6f06531d0ce73e74
James Oulman <joulman@gmail.com>
2022-08-15 11:41:16 -0400

    create and sign ca certs

--- vault-config/pki.tf
+++ vault-config/pki.tf
@@ -27,6 +27,7 @@ resource "vault_mount" "pki_int" {
   max_lease_ttl_seconds = 60 * 60 * 24 * 365
 }
 
+# Generate the Root CA cert, sign it, set it
 resource "vault_pki_secret_backend_config_urls" "pki_int_config_urls" {
   backend                 = vault_mount.pki_int.path
   issuing_certificates    = ["${data.terraform_remote_state.vault.outputs.hcp_vault_private_endpoint_url}/v1/${vault_mount.pki_int.path}/ca"]
@@ -43,13 +44,23 @@ resource "vault_pki_secret_backend_root_cert" "pki_root_cert" {
   ttl         = 60 * 60 * 24 * 365 * 10
 }
 
+## Generate the cert request, sign it, set it
+resource "vault_pki_secret_backend_intermediate_cert_request" "pki_int" {
+  depends_on = [vault_mount.pki_int]
+
+  backend = vault_mount.pki_int.path
+
+  type        = "internal"
+  common_name = "Boundary Lab Intermediate CA"
+}
+
 resource "vault_pki_secret_backend_root_sign_intermediate" "pki_root_int" {
   depends_on = [vault_pki_secret_backend_intermediate_cert_request.pki_int]
 
   backend = vault_mount.pki_root.path
 
   csr         = vault_pki_secret_backend_intermediate_cert_request.pki_int.csr
-  common_name = "Boundary Lab Intermediary CA"
+  common_name = "Boundary Lab Intermediate CA"
   format      = "pem_bundle"
   ttl         = 60 * 60 * 24 * 365
 }
