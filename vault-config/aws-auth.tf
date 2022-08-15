b9a7cb55534427ea679e6193fde08fba6da6f182
James Oulman <joulman@gmail.com>
2022-08-15 16:03:25 -0400

    fix vault policy arn

--- vault-config/aws-auth.tf
+++ vault-config/aws-auth.tf
@@ -16,16 +16,15 @@ data "aws_iam_policy_document" "hcpvault_policy" {
   }
 }
 
-resource "aws_iam_user_policy" "hcp_vault_auth_policy" {
+resource "aws_iam_policy" "hcp_vault_auth_policy" {
   name = "hcp-vault-auth-policy"
-  user = aws_iam_user.hcp_vault.name
 
   policy = data.aws_iam_policy_document.hcpvault_policy.json
 }
 
 resource "aws_iam_user_policy_attachment" "hcp_vault_attach" {
   user       = aws_iam_user.hcp_vault.name
-  policy_arn = aws_iam_user_policy.hcp_vault_auth_policy.arn
+  policy_arn = aws_iam_policy.hcp_vault_auth_policy.arn
 }
 
 resource "aws_iam_access_key" "hcp_vault" {
